9d32efe7f8ec05c3841023618282f36f9a0ab5da
weiwei <astrid062548@gmail.com>
2024-09-15 10:38:44 -0700

    - Add a <TextInput> in App.js. - Make the TextInput a controlled component. - Add a <Text> component in App.js. Show the text that that user enters in the TextInput in this component.

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 export default function App() {
   const appName = 'my first react native app';
   
+  const [inputText, setInputText] = useState('');
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
   return (
     <View style={styles.container}>
       {/* <Text>Welcome to {appName}!</Text> */}
       <Header appName={appName} />
+
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        placeholder='Type here!'
+        onChangeText={handleInputChange}
+        value={inputText}
+      />
+     <Text style={styles.output}>
+        You entered: {inputText}
+      </Text>
+
+      
       <StatusBar style="auto" />
     </View>
   );
