f3fcc0f403843e56d9d9d73a76d3ae9b376d57d8
weiwei <astrid062548@gmail.com>
2024-09-15 10:27:04 -0700

    passing app_name prop to header.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
   const appName = 'my first react native app';
   
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}!</Text>
+      {/* <Text>Welcome to {appName}!</Text> */}
+      <Header appName={appName} />
       <StatusBar style="auto" />
     </View>
   );
