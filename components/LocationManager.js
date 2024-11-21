8dd30d32fc3e3f17c29223168c211230f30afe74
weiwei <astrid062548@gmail.com>
2024-11-21 11:27:18 -0800

    set the initial region based on the location found by locate me button

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -7,25 +7,31 @@ import {
   Text,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+// import { getCurrentPositionAsync } from "expo-location";
 import * as Location from "expo-location";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 const windowWidth = Dimensions.get("window").width;
 
 export default function LocationManager() {
-  const navigation = useNavigation();
-
   const [location, setLocation] = useState(null);
   const [response, requestPermission] = Location.useForegroundPermissions();
+  const navigation = useNavigation();
+  const route = useRoute();
+  useEffect(() => {
+    if (route.params && route.params.selectedLocation) {
+      setLocation(route.params.selectedLocation);
+    }
+  }, [route]);
   async function verifyPermission() {
+    //check if user has granted permission return true
     try {
-      //check if user has given permission
-      //if so return true
       if (response.granted) {
         return true;
       }
-      //if not ask for permission and return what user has chosen
       const permissionResponse = await requestPermission();
+      //else ask for permission
+      //return the granted property of the response
       return permissionResponse.granted;
     } catch (err) {
       console.log("verify permission ", err);
@@ -33,36 +39,46 @@ export default function LocationManager() {
   }
   async function locateUserHandler() {
     try {
+      // call verifyPermission and only proceed if you have permission
       const hasPermission = await verifyPermission();
       if (!hasPermission) {
-        Alert.alert("You need to give location permission");
+        Alert.alert("YOu need to give location permission");
         return;
       }
-      const response = await Location.getCurrentPositionAsync();
+      const locationResponse = await Location.getCurrentPositionAsync();
+      console.log(locationResponse);
       setLocation({
-        latitude: response.coords.latitude,
-        longitude: response.coords.longitude,
+        latitude: locationResponse.coords.latitude,
+        longitude: locationResponse.coords.longitude,
       });
     } catch (err) {
       console.log("locate user ", err);
     }
   }
+  function chooseLocationHandler() {
+    //navigate to Map.js
+    //check if location is not null, pass it to Map
+    if (location) {
+      navigation.navigate("Map", { initialLocation: location });
+    } else {
+      navigation.navigate("Map");
+    }
+  }
+
   return (
     <View>
       <Button title="Locate Me" onPress={locateUserHandler} />
       <Button
-        title="Let me choose on the map"
-        onPress={() => {
-          //navigate to Map screen
-          navigation.navigate("Map");
-        }}
+        title="Let me choose my location"
+        onPress={chooseLocationHandler}
       />
       {location && (
         <Image
           source={{
             uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${process.env.EXPO_PUBLIC_mapsApiKey}`,
           }}
-          style={styles.image}
+          style={styles.map}
+          alt="static map"
         />
       )}
     </View>
@@ -70,5 +86,5 @@ export default function LocationManager() {
 }
 
 const styles = StyleSheet.create({
-  image: { width: windowWidth, height: 200 },
+  map: { width: windowWidth, height: 200 },
 });
